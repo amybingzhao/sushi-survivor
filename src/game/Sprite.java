e00ba033fae395eeaf602475b90fb15517c67f1b
azhao96 <azhao96@gmail.com>
2016-01-22 18:05:56 -0500

    added comments to all methods in sprite.java and sushi.java

--- src/game/Sprite.java
+++ src/game/Sprite.java
@@ -13,65 +13,110 @@ public class Sprite {
 	private double height;
 	private double width;
 	
+	/*
+	 * Sets the image for the sprite.
+	 */
 	public void setImage(String filename) {
 		image = new Image(getClass().getClassLoader().getResourceAsStream(filename));
 		setHeight(image.getHeight());
 		setWidth(image.getWidth());
 	}
 	
+	/*
+	 * Sets the (x,y) position of the sprite.
+	 */
 	public void setPosition(double x, double y) {
 		setPosX(x);
 		setPosY(y);
 	}
 	
+	/*
+	 * Draws the sprite on the canvas.
+	 */
 	public void render(GraphicsContext gc) {
 		gc.drawImage(getImage(), getPosX(), getPosY());
 	}
 	
+	/*
+	 * Returns the boundary of the sprite image.
+	 */
 	public Rectangle2D getBoundary() {
 		return new Rectangle2D(getPosX(), getPosY(), getWidth(), getHeight());
 	}
 	
+	/*
+	 * Returns true if the sprite image intersects another sprite's boundary.
+	 */
 	public boolean intersects(Rectangle2D r) {
 		return r.intersects(this.getBoundary());
 	}
 
+	/*
+	 * Returns the y-position of the sprite.
+	 */
 	public double getPosY() {
 		return posY;
 	}
 
+	/*
+	 * Sets the y-position of the sprite.
+	 */
 	public void setPosY(double posY) {
 		this.posY = posY;
 	}
 
+	/*
+	 * Returns the x-position of the sprite.
+	 */
 	public double getPosX() {
 		return posX;
 	}
-
+	
+	/*
+	 * Sets the x-position of the sprite.
+	 */
 	public void setPosX(double posX) {
 		this.posX = posX;
 	}
 
+	/*
+	 * Returns the sprite's image.
+	 */
 	public Image getImage() {
 		return image;
 	}
 
+	/*
+	 * Sets the sprite's image.
+	 */
 	public void setImage(Image image) {
 		this.image = image;
 	}
 
+	/*
+	 * Returns the sprite's height.
+	 */
 	public double getHeight() {
 		return height;
 	}
 
+	/*
+	 * Sets the sprite's height.
+	 */
 	public void setHeight(double height) {
 		this.height = height;
 	}
 
+	/*
+	 * Returns the sprite's width.
+	 */
 	public double getWidth() {
 		return width;
 	}
-
+	
+	/*
+	 * Gets the sprite's height.
+	 */
 	public void setWidth(double width) {
 		this.width = width;
 	}
--- src/game/Sushi.java
+++ src/game/Sushi.java
@@ -24,6 +24,9 @@ public class Sushi extends Sprite {
 		setWidth(getImage().getWidth());
 	}
 
+	/*
+	 * Handles Sushi sprite's movement in response to player key input.
+	 */
 	public void handleInput(ArrayList<String> input, double canvasWidth, double canvasHeight, String level) {
 		if (input.contains("LEFT") && (getPosX() - speed >= 0)) {
 			setPosX(getPosX() - speed);
@@ -52,6 +55,9 @@ public class Sushi extends Sprite {
 		}
 	}
 	
+	/*
+	 * Sets the Sushi sprite's speed.
+	 */
 	public void setSpeed(double s) {
 		if (s >= 0) {
 			this.speed = s;
@@ -60,14 +66,23 @@ public class Sushi extends Sprite {
 		}
 	}
 	
+	/*
+	 * Sets the Sushi sprite's number of fish.
+	 */
 	public void setNumFish(double n) {
 		this.numFish = n;
 	}
 	
+	/*
+	 * Returns the Sushi sprite's speed.
+	 */
 	public double getSpeed() {
 		return this.speed;
 	}
 	
+	/*
+	 * Returns the Sushi sprite's current number of fish.
+	 */
 	public double getNumFish() {
 		return this.numFish;
 	}
